55d3c3407dcaedd22ee4f41eb62b28c9ece2f6a8
Rémy Portelas <remy.portelas@gmail.com>
2018-06-22 10:39:30 +0200

    add multi-run script

---
+++ run_campain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+malmo_folder="Malmo-0.34.0-Linux-Ubuntu-16.04-64bit_withBoost_Python2.7/"
+echo "Bash version ${BASH_VERSION}..."
+for ((i = 0; i < $1; i++)); do
+    echo $i
+    #xterm -title "app $i" -e "./launchclient & ; sleep 15"
+    #./launchClient.sh  > malmo_out$i.txt 2>&1 &
+    port=$((10000 + $i))
+    echo hello
+    echo $port
+    python flowers_minecart.py test$i random_flat 10000 1000 0.1 $port &
+    #> run$i_output.txt 2>&1 &
+done
