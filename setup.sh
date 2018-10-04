915ed47046c360f0db49a6f4619d233e0b1b360d
Rémy Portelas <remy.portelas@gmail.com>
2018-10-04 15:19:20 +0200

    add MC opti in setup

--- setup.sh
+++ setup.sh
@@ -22,4 +22,7 @@ echo $malmo_folder > minecraft_version_config.txt
 #Copy Malmo lib
 cp -R ~/$malmo_folder/Python_Examples/MalmoPython.so gym2/envs/malmo/
 cp -R ~/$malmo_folder/Python_Examples/MalmoPython.so .
-source ~/.bashrc
+
+#Copy Minecraft optimized configuration
+cp options.txt ~/$malmo_folder/Minecraft/run/
+echo "You can now source your bashrc"
