1407aa9665a4ba1188439389085749e975f0f73b
Rémy Portelas <remy.portelas@gmail.com>
2018-10-04 11:25:32 +0200

    python3 on run campain

--- run_campain.sh
+++ run_campain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for ((i = 0; i < $1; i++)); do
-    python imgep_in_mmc_controller.py test_rmb_$i random_modular 10000 1000 &
-    python imgep_in_mmc_controller.py test_amb_$i active_modular 10000 1000 &
-    python imgep_in_mmc_controller.py test_f_rgb_$i random_flat 10000 1000 &
-    python imgep_in_mmc_controller.py test_random_$i random_flat 10000 10000 &
+    python3 imgep_in_mmc_controller.py test_rmb_$i random_modular &
+    python3 imgep_in_mmc_controller.py test_amb_$i active_modular &
+    python3 imgep_in_mmc_controller.py test_f_rgb_$i random_flat &
+    python3 imgep_in_mmc_controller.py test_random_$i random_flat 10000 10000 &
     #> run$i_output.txt 2>&1 &
 done
