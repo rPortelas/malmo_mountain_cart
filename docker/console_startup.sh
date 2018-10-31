3089d1344304f0dbe24a9dd3e715449c72cdea0f
rPortelas <remy.portelas@gmail.com>
2018-10-31 17:39:08 +0100

    docker with xpra

--- docker/console_startup.sh
+++ docker/console_startup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-/dockerstartup/vnc_startup.sh echo "Starting Malmo Minecraft Mod"
 cd /home/malmo
-# Launch mimecraft (which may take several minutes first time)
-#python3 -c "import malmo.minecraftbootstrap;malmo.minecraftbootstrap.launch_minecraft()"
 cd MalmoPlatform/Minecraft/
 echo "WILL BE USING PORT $1"
-xvfb-run -a -e /dev/stdout -s '-screen 0 1400x900x24' ./launchClient.sh -port $1
-#echo "Starting jupyter TEST MALMO"
-#python3 test_malmo.py
-#jupyter notebook --ip 0.0.0.0 --no-browser
+xpra start :100
+export DISPLAY=:100
+./launchClient.sh -port $1
